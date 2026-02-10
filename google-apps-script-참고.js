ebb36211237fff7ec688c61e1eb1f4d46a11f4cb
hajjanggun-cyber <hajjanggun-cyber@users.noreply.github.com>
2026-02-10 11:30:07 +0900

    Add Lectures category

--- google-apps-script-참고.js
+++ google-apps-script-참고.js
@@ -22,7 +22,7 @@ function doGet(e) {
 
 // 모든 시트의 데이터를 싹 긁어서 객체로 반환 (성능 최적화용)
 function loadAllData() {
-  const sheets = ['Terms', 'Commands', 'Videos'];
+  const sheets = ['Terms', 'Commands', 'Videos', 'Ideas', 'Lectures'];
   const result = {};
 
   sheets.forEach(name => {
